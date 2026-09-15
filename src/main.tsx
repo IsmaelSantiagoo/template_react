ecd1e093fb308f05fe5c2ce2b7c3ed3efcc86846
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2026-09-15 14:34:36 -0300

    feat: add Montserrat font support and update CSS for theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './app'
 import './index.css'
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
 
 createRoot(document.getElementById('root')!).render(<App />)
